8010a7fbf2dddb27f1722048df24cb6f958105a1
David Arambulo <davida.arambuloa@gmail.com>
2021-09-03 17:30:03 -0300

    Sacamos el array de recetas a un json a parte y despues usamos ese archivo para obtener la data, se agregaron metodos del local y el session storage al readme

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ El código desarrollado en cada una de las clases lo podemos encontra en ramas
 
 
 Hice un cambio lo tienes que copiar en tu PC
+
+## Metodos del Local y el Session Storage
+
+- sessionStorage
+  localStorage
+      .getItem(Clave)
+      .setItem(Clave, valor)
+      .clear()
+      .removeItem(Clave)
+      .length()
+      .key(Indice)
+- JSON *Estos se usan para traducir de JSON a Objeto y e Objeto a JSON*
+      .parse(Objeto en JSON)
+      .stringify(Objeto)
--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,22 @@
-const recetas = [
-    {
-        id: 0,
-        nombre: 'receta0',
-        preparacion: 'preparacion',
-        ingredientes: ['ingrediente1', 'ingrediente2']
-    },
-    {
-        id: 1,
-        nombre: 'receta1',
-        preparacion: 'preparacion',
-        ingredientes: ['ingrediente1', 'ingrediente2']
-    },
-    {
-        id: 2,
-        nombre: 'receta2',
-        preparacion: 'preparacion',
-        ingredientes: ['ingrediente1', 'ingrediente2']
-    },
-    {
-        id: 3,
-        nombre: 'receta3',
-        preparacion: 'preparacion',
-        ingredientes: ['ingrediente1', 'ingrediente2']
-    },
-]
+// EL SIGUIENTE BLOQUE DE CODIGO ES PARA TRAER UN JSON EXTERNOS
+
+// Esta URL la saque de aqui: https://myjson.dit.upm.es/1i9k es una página que nos permite publicar nuestro JSON
+const recetasURL = 'http://myjson.dit.upm.es/api/bins/1i9k'
+
+// Inicializo la variable antes para no tener errores en el codigo 
+let recetas = []
+
+// Fetch lo que hace es traer la data de una API (piensen una API como una especie de base de datos en la nube)
+fetch(recetasURL)
+    // then lo que hace es esperar a que haga la conexion con la API y cuando lo hace ejecuta una funcion
+    .then(function (respuesta) {
+        return respuesta.json()
+    })
+    // Este then espera a que termine de ejecutarse el then anterior y guarda la data en la variable
+    .then(function (data) {
+        recetas = data
+    })
+
 
 const catalogoRecetas = document.getElementById('catalogo-de-recetas')
 const btnMostrar = document.getElementById('btn-mostrar')
